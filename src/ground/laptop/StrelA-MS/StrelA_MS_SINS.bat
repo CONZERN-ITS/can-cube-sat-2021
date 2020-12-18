b0014f37a9030058bd41260c4edf6992f7e58907
Agnus290 <tenebris.agnus@yandex.ru>
2020-12-18 20:21:02 +0300

    Батники для запуска стрелы

---
+++ src/ground/laptop/StrelA-MS/StrelA_MS_SINS.bat
@@ -0,0 +1,3 @@
+@echo off
+set "STRELA_MS_CONFIG=./config_templates/StrelA_MS_SINS.ini"
+python.exe main.py
---
+++ src/ground/laptop/StrelA-MS/StrelA_MS_STATE.bat
@@ -0,0 +1,3 @@
+@echo off
+set "STRELA_MS_CONFIG=./config_templates/StrelA_MS_STATE.ini"
+python.exe main.py
