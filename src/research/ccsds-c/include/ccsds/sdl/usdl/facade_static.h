fadaff67d673310fd7a3857d08b602baee3202f7
SeresHotes <sereshotes@gmail.com>
2021-02-19 17:51:32 +0300

    Последние нововедения во внешний рефакторинг

--- src/research/ccsds-c/include/ccsds/sdl/usdl/facade_static.h
+++ src/research/ccsds-c/include/ccsds/sdl/usdl/facade_static.h
@@ -29,6 +29,15 @@ typedef struct usdl_t {
 	size_t packet_size;
 } usdl_t;
 
+typedef void *sap_t;
+
+sap_t usdl_get_map_sap(usdl_t *usdl, mc_id_t mc_id, vc_id_t vc_id, map_id_t map_id);
+sap_t usdl_get_vc_sap(usdl_t *usdl, mc_id_t mc_id, vc_id_t vc_id);
+sap_t usdl_get_mc_sap(usdl_t *usdl, mc_id_t mc_id);
+int usdl_mapp_push(sap_t sap, const uint8_t *data, size_t size, pvn_t pvn, quality_of_service_t qos);
+int usdl_mapa_push(sap_t sap, const uint8_t *data, size_t size, quality_of_service_t qos);
+
+
 #define USDL_STATIC_ALLOCATE(map_count, vc_count, mc_count, packet_size, frame_size) \
 	struct { \
 		usdl_t base; \
@@ -56,9 +65,6 @@ typedef struct usdl_t {
 		usdl.mx_pool_size = sizeof(usdl_static.mx_pool) / sizeof(usdl_static.mx_pool[0]); \
 	} while (0);
 
-mc_t *_find_mc(const usdl_t *usdl, mc_id_t mc_id);
-vc_t *_find_vc(const usdl_t *usdl, mc_id_t mc_id, vc_id_t vc_id);
-map_t *_find_map(const usdl_t *usdl, mc_id_t mc_id, vc_id_t vc_id, map_id_t map_id);
 
 void usdl_vc_init(usdl_t *usdl, const vc_parameters_t *params, mc_id_t mc_id, vc_id_t vc_id);
 void usdl_mc_init(usdl_t *usdl, const mc_paramaters_t *params, mc_id_t mc_id);
--- src/research/ccsds-c/src/ccsds/sdl/usdl/facade_static.c
+++ src/research/ccsds-c/src/ccsds/sdl/usdl/facade_static.c
@@ -15,52 +15,6 @@
 #include <ccsds/sdl/usdl/multiplexer.h>
 #include <ccsds/sdl/usdl/usdl_types.h>
 
-typedef struct usdl_t {
-	pc_t *pc;
-	mc_t *mc_pool;
-	size_t mc_pool_size;
-	vc_t *vc_pool;
-	size_t vc_pool_size;
-	map_t *map_pool;
-	size_t map_pool_size;
-	struct multiplexer_abstract *mx_pool;
-	size_t mx_pool_size;
-	uint8_t *is_free;
-	size_t is_free_size;
-	uint8_t *frame_buf_pool;
-	size_t frame_size;
-	uint8_t *packet_buf_pool;
-	size_t packet_size;
-} usdl_t;
-
-#define USDL_STATIC_ALLOCATE(map_count, vc_count, mc_count, packet_size, frame_size) \
-	struct { \
-		usdl_t base; \
-		map_t map_pool[map_count]; \
-		vc_t vc_pool[vc_count]; \
-		mc_t mc_pool[mc_count]; \
-		pc_t pc; \
-		struct multiplexer_rr mx_pool[(vc_count) + (mc_count)]; \
-		uint8_t is_free[(map_count) + (vc_count) + (mc_count) + (vc_count) + (mc_count)]; \
-		uint8_t frame_buf_pool[(map_count) * (frame_size)]; \
-		uint8_t packet_buf_pool[(1 + (vc_count)) * (packet_size)]; \
-	}
-#define USDL_STATIC_INIT(usdl_static, usdl) do { \
-		usdl.pc = &usdl_static.pc; \
-		usdl.mc_pool = usdl_static.mc_pool; \
-		usdl.vc_pool = usdl_static.vc_pool; \
-		usdl.map_pool = usdl_static.map_pool; \
-		usdl.mx_pool = (struct multiplexer_abstract *)usdl_static.mx_pool; \
-		usdl.frame_buf_pool = usdl_static.frame_buf_pool; \
-		usdl.packet_buf_pool = usdl_static.packet_buf_pool; \
-		\
-		usdl.mc_pool_size = sizeof(usdl_static.mc_pool) / sizeof(usdl_static.mc_pool[0]); \
-		usdl.vc_pool_size = sizeof(usdl_static.vc_pool) / sizeof(usdl_static.vc_pool[0]); \
-		usdl.map_pool_size = sizeof(usdl_static.map_pool) / sizeof(usdl_static.map_pool[0]); \
-		usdl.mx_pool_size = sizeof(usdl_static.mx_pool) / sizeof(usdl_static.mx_pool[0]); \
-	} while (0);
-#define usdl_static_TEMPLATE_TO_usdl_static(usdl_static) usdl_static.base;
-
 enum layer_t {
 	LAYER_MAP,
 	LAYER_VC,
@@ -168,4 +122,19 @@ void usdl_init() {
 	USDL_STATIC_ALLOCATE(10, 4, 1, 1000, 100) usdl_static;
 	usdl_t usdl = {0};
 	USDL_STATIC_INIT(usdl_static, usdl);
+}
+sap_t usdl_get_map_sap(usdl_t *usdl, mc_id_t mc_id, vc_id_t vc_id, map_id_t map_id) {
+	return _find_map(usdl->pc, mc_id, vc_id, map_id);
+}
+sap_t usdl_get_vc_sap(usdl_t *usdl, mc_id_t mc_id, vc_id_t vc_id) {
+	return _find_vc(usdl->pc, mc_id, vc_id);
+}
+sap_t usdl_get_mc_sap(usdl_t *usdl, mc_id_t mc_id) {
+	return _find_mc(usdl->pc, mc_id);
+}
+int usdl_mapp_push(sap_t sap, const uint8_t *data, size_t size, pvn_t pvn, quality_of_service_t qos) {
+	return mapp_send(sap, data, size, pvn, qos);
+}
+int usdl_mapa_push(sap_t sap, const uint8_t *data, size_t size, quality_of_service_t qos) {
+	return mapa_send(sap, data, size, qos);
 }
