5a5171ff81cbf174c743f0a196a9882d951ba703
dll31 <danila.litvinov31@yandex.ru>
2021-05-02 21:57:40 +0300

    проба баланса сообщений. необходимо доделать

--- src/board/SINS/Application/drivers/uplink.c
+++ src/board/SINS/Application/drivers/uplink.c
@@ -9,14 +9,20 @@
 
 #include <stm32f4xx_hal.h>
 #include <its-i2c-link.h>
+#include <errno.h>
 
 #include "common.h"
+
+mavlink_message_t high_priority_msg;
+int8_t flag_have_high_priority_msg = 0;
+
 
 int uplink_init()
 {
 	int error = its_i2c_link_start();
 	return error;
 }
+
 
 int uplink_write_raw(const void * data, int data_size)
 {
@@ -30,8 +36,25 @@ int uplink_write_mav(const mavlink_message_t * msg)
 	// https://mavlink.io/en/about/overview.html#mavlink-2-packet-format
 	static uint8_t msg_buffer[280]; // 280 максимальный размер MAV пакета версии 2
 
+	int error = 0;
+	if (0 != flag_have_high_priority_msg)
+	{
+		uint16_t len = mavlink_msg_to_send_buffer(msg_buffer, &high_priority_msg);
+		error = uplink_write_raw(msg_buffer, len);
+		flag_have_high_priority_msg = 0;
+	}
+
 	uint16_t len = mavlink_msg_to_send_buffer(msg_buffer, msg);
-	int error = uplink_write_raw(msg_buffer, len);
+	error = uplink_write_raw(msg_buffer, len);
+	if (error == -EAGAIN)
+	{
+		if (msg->msgid != MAVLINK_MSG_ID_SINS_isc)
+		{
+			flag_have_high_priority_msg = 1;
+			memcpy(&high_priority_msg, msg, sizeof(*msg));
+		}
+	}
+
 	if (error)
 		return error;
 
