0a6cc7bb224fb5c6ea33b14565f9d0c542032851
SeresHotes <sereshotes@gmail.com>
2021-05-05 18:42:08 +0300

    Укорачиваем Солнечный датчик

--- src/research/lds_on_stm32f407/App/Src/tmain.c
+++ src/research/lds_on_stm32f407/App/Src/tmain.c
@@ -38,36 +38,35 @@ int _write(int file, char *ptr, int len)
 #define LDS_COUNT 3
 #define LDS_DIM 3
 
-float Arr[3][3] = {
+float Arr[3][LDS_COUNT] = {
         {1.00000, 0.00000, 0.00000 },
         {-1.00000, 0.75249, 0.75249 },
         {0.00000, -1.46190, 1.46190 }
 };
+
+
+void lds_find(float x[3], float b[LDS_COUNT]) {
+    for (int i = 0; i < 3; i++) {
+        x[i] = 0;
+        for (int j = 0; j < LDS_COUNT; j++) {
+            x[i] += Arr[i][j] * b[j];
+        }
+    }
+}
+
+void dekart_to_euler(float x[3], float sph[3]) {
+    sph[0] = sqrt(x[0] * x[0] + x[1] * x[1] + x[2] * x[2]);
+    sph[1] = acos(x[2] / sph[0]);
+    sph[2] = atan(x[1] / x[0]);
+}
 
 float degrees(float a) {
     return a * 180 / M_PI;
 }
 
 int tmain() {
-    Matrixf x;
-    Matrixf b;
-    lds_t lds;
-    Matrixf temp;
-
-
-    MATRIX_SA(x, LDS_DIM, 1);
-    MATRIX_SA(b, LDS_COUNT, 1);
-    MATRIX_SA(temp, LDS_DIM, LDS_DIM);
-    MATRIX_SA(lds.A, LDS_COUNT, LDS_DIM);
-    MATRIX_SA(lds.As, LDS_DIM, LDS_COUNT);
-
-    Matrixf arr;
-    arr.arr = (float *)Arr;
-    arr.height = 3;
-    arr.width = 3;
-    arr.reserved = 9;
-    lds_init(&lds, LDS_COUNT, LDS_DIM, &temp);
-    hadc1.Instance->SQR3 = 0;
+    float x[3];
+    float b[LDS_COUNT];
     int step = 0;
     while (1) {
         for (int i = 0; i < LDS_COUNT; i++) {
@@ -77,19 +76,19 @@ int tmain() {
             uint32_t value = HAL_ADC_GetValue(&hadc1);
             float v = ((float)value / (1 << 12)) * 3.3;
 
-            *matrix_at(&b, i, 0) = v;
+            b[i] = v;
         }
-        matrix_multiplicate(&arr, &b, &x);
-/*
-        lds_search(&lds, &b, &x);
-        float err = lds_get_error(&lds, &b, &x);
-*/
 
-        float mag = matrix_norm(&x);
-        float phi = atan(*matrix_at(&x, 1, 0) / *matrix_at(&x, 0, 0));
-        float psi = acos(*matrix_at(&x, 2, 0) / mag);
-        printf("a1: %4.4f: a2: %4.4f a3: %4.4f\n", *matrix_at(&b, 0, 0), *matrix_at(&b, 1, 0), *matrix_at(&b, 2, 0));
-        printf("%4d: mag: %4.4f phi: %.4f psi: %.4f\n", step, mag, degrees(phi), degrees(psi));
+        float sph[3];
+        lds_find(x, b);
+        dekart_to_euler(x, sph);
+
+        for (int i = 0; i < LDS_COUNT; i++) {
+            printf("a%d: %f0.3 ", i, b[i]);
+        }
+        printf("\n");
+
+        printf("%4d: mag: %4.4f theta: %.4f phi: %.4f\n", step, sph[0], degrees(sph[1]), degrees(sph[2]));
         HAL_Delay(1000);
         HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
         step++;
