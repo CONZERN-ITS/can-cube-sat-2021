8e7435a099e8f84a601076e36fa068cdf716a3ae
snork2 <strange.snork@gmail.com>
2021-05-24 23:45:21 +0300

    Вспомогательное приложение для проверки радио-линии. Гонит натуральный ряд по радио

---
+++ src/research/lora-f4-spamer/Application/app_main.c
@@ -0,0 +1,214 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <stm32f4xx_hal.h>
+
+#include "sx126x_drv.h"
+
+#define APP_PACKET_SIZE 200
+
+
+static sx126x_drv_t _radio;
+static uint8_t _pbuff[255] = {0};
+static uint8_t _psize;
+
+
+static int _push_packet(sx126x_drv_t * radio)
+{
+	int rc;
+	static uint8_t serial = 0;
+	for (size_t i = 0; i < APP_PACKET_SIZE; i++)
+		_pbuff[i] = serial++;
+
+	_psize = APP_PACKET_SIZE;
+
+	rc = sx126x_drv_payload_write(radio, _pbuff, _psize);
+	if (0 != rc) return rc;
+
+	return 0;
+}
+
+
+static int _fetch_packet(sx126x_drv_t * radio)
+{
+	int rc;
+
+	rc = sx126x_drv_payload_rx_size(radio, &_psize);
+	if (0 != rc) return rc;
+
+	// К сожалению яхз как вытащить фактическую цифру из радио
+	_psize = APP_PACKET_SIZE;
+
+	rc = sx126x_drv_payload_read(radio, _pbuff, _psize);
+	if (0 != rc) return rc;
+
+	printf("got packet %d\n", (int)_pbuff[0]);
+	return 0;
+}
+
+
+static void _event_handler(sx126x_drv_t * drv, void * user_arg, sx126x_evt_kind_t kind, const sx126x_evt_arg_t * arg)
+{
+	int rc = 0;
+	uint32_t now = HAL_GetTick();
+
+	switch (kind)
+	{
+	case SX126X_EVTKIND_TX_DONE:
+		printf("%d tx done\n", (int)now);
+		if (arg->tx_done.timed_out)
+			printf("tx timed out!\n");
+
+		rc = sx126x_drv_mode_standby(drv);
+		if (0 != rc) break;
+
+		// Уходим в цикл RX
+		rc = sx126x_drv_mode_rx(drv, 5000);
+		break;
+
+	case SX126X_EVTKIND_RX_DONE:
+		if (!arg->rx_done.timed_out)
+		{
+			printf(
+				"%d, rx done. crc: %s, prssi %d, snr: %d, srssi: %d\n",
+				(int)now,
+				arg->rx_done.crc_valid ? "true" : "false",
+				arg->rx_done.packet_status.lora.rssi_pkt,
+				arg->rx_done.packet_status.lora.signal_rssi_pkt,
+				arg->rx_done.packet_status.lora.snr_pkt
+			);
+			rc = _fetch_packet(drv);
+			if (0 != rc) break;
+
+		}
+		rc = sx126x_drv_mode_standby(drv);
+		if (0 != rc) break;
+
+		rc = _push_packet(drv);
+		if (0 != rc) break;
+
+		rc = sx126x_drv_mode_tx(drv, 0);
+		if (0 != rc) break;
+		break;
+
+	case SX126X_EVTKIND_CAD_DONE:
+		printf("%d cad done?\n", (int)now);
+		break;
+
+	default:
+		printf("unknown event: %d\n", kind);
+	};
+
+	if (0 != rc)
+		printf("event handler error: %d\n", rc);
+}
+
+
+int app_main(void)
+{
+	int rc;
+
+	const sx126x_drv_basic_cfg_t basic_cfg = {
+			.use_dio3_for_tcxo = true,
+			.tcxo_v = SX126X_TCXO_CTRL_1_8V,
+			.use_dio2_for_rf_switch = false,
+			.allow_dcdc = true,
+			.standby_mode = SX126X_STANDBY_XOSC
+	};
+
+	const sx126x_drv_lora_modem_cfg_t modem_cfg = {
+			// Параметры усилителей и частот
+			.frequency = 435125*1000,
+			.pa_ramp_time = SX126X_PA_RAMP_1700_US,
+			.pa_power = 10,
+			.lna_boost = true,
+
+			// Параметры пакетирования
+			.spreading_factor = SX126X_LORA_SF_6,
+			.bandwidth = SX126X_LORA_BW_250,
+			.coding_rate = SX126X_LORA_CR_4_8,
+			.ldr_optimizations = false,
+	};
+
+	const sx126x_drv_lora_packet_cfg_t packet_cfg = {
+			.invert_iq = false,
+			.syncword = SX126X_LORASYNCWORD_PRIVATE,
+			.preamble_length = 24,
+			.explicit_header = true,
+			.payload_length = APP_PACKET_SIZE,
+			.use_crc = true,
+	};
+
+	const sx126x_drv_cad_cfg_t cad_cfg = {
+			.cad_len = SX126X_LORA_CAD_04_SYMBOL,
+			.cad_min = 10,
+			.cad_peak = 28,
+			.exit_mode = SX126X_LORA_CAD_RX,
+	};
+
+	const sx126x_drv_lora_rx_timeout_cfg_t rx_timeout_cfg = {
+			.stop_timer_on_preable = false,
+			.lora_symb_timeout = 255 //24*10
+	};
+
+	rc = sx126x_drv_ctor(&_radio, NULL);
+	printf("ctor: %d\n", rc);
+
+	rc = sx126x_drv_register_event_handler(&_radio, _event_handler, NULL);
+	printf("register_event_handler: %d\n", rc);
+
+	rc = sx126x_drv_reset(&_radio);
+	printf("reset: %d\n", rc);
+
+	rc = sx126x_drv_mode_standby_rc(&_radio);
+	printf("standy_rc: %d\n", rc);
+
+	rc = sx126x_drv_configure_basic(&_radio, &basic_cfg);
+	printf("configure_basic: %d\n", rc);
+
+	rc = sx126x_drv_configure_lora_modem(&_radio, &modem_cfg);
+	printf("configure_lora_modem: %d\n", rc);
+
+	rc = sx126x_drv_configure_lora_packet(&_radio, &packet_cfg);
+	printf("configure_lora_packet: %d\n", rc);
+
+	rc = sx126x_drv_configure_lora_cad(&_radio, &cad_cfg);
+	printf("configure_cad: %d\n", rc);
+
+	rc = sx126x_drv_configure_lora_rx_timeout(&_radio, &rx_timeout_cfg);
+	printf("configure_timeout: %d\n", rc);
+
+	// Начинаем с того, что начинаем приём
+	rc = _push_packet(&_radio);
+	printf("first packet pushed: %d\n", rc);
+
+	rc = sx126x_drv_mode_standby(&_radio);
+	printf("went to standby: %d\n", rc);
+
+	sx126x_status_t status;
+	rc = sx126x_api_get_status(&_radio.api, &status);
+	printf("status read; rc=%d, cmd_status=%d, dev_status=%d\n",
+			rc, (int)status.bits.cmd_status, (int)status.bits.chip_mode
+	);
+
+	rc = sx126x_drv_mode_tx(&_radio, 0);
+	printf("first tx: %d\n", rc);
+
+	rc = sx126x_api_get_status(&_radio.api, &status);
+	printf("status read; rc=%d, cmd_status=%d, dev_status=%d\n",
+			rc, (int)status.bits.cmd_status, (int)status.bits.chip_mode
+	);
+
+	while(1)
+	{
+		rc = sx126x_drv_poll(&_radio);
+		if (rc)
+			printf("poll error: %d\n", rc);
+
+		HAL_Delay(50);
+	}
+
+	return 0;
+}
---
+++ src/research/lora-f4-spamer/Application/sx126x_board.c
@@ -0,0 +1,259 @@
+#include <string.h>
+
+#include <stm32f4xx_hal.h>
+
+#include "main.h"
+#include "sx126x_board.h"
+
+
+extern SPI_HandleTypeDef hspi1;
+#define hspi (&hspi1)
+
+
+#define SX126X_TXE_GPIO_Port GPIOC
+#define SX126X_TXE_Pin GPIO_PIN_4
+
+#define SX126X_RXE_GPIO_Port GPIOC
+#define SX126X_RXE_Pin GPIO_PIN_5
+
+#define SX126X_CS_GPIO_Port GPIOB
+#define SX126X_CS_Pin GPIO_PIN_0
+
+#define SX126X_NRST_GPIO_Port GPIOB
+#define SX126X_NRST_Pin GPIO_PIN_1
+
+#define SX126X_BUSY_GPIO_Port GPIOE
+#define SX126X_BUSY_Pin GPIO_PIN_7
+
+
+static void _cs_down(void)
+{
+	HAL_GPIO_WritePin(SX126X_CS_GPIO_Port, SX126X_CS_Pin, GPIO_PIN_RESET);
+}
+
+
+static void _cs_up(void)
+{
+	HAL_GPIO_WritePin(SX126X_CS_GPIO_Port, SX126X_CS_Pin, GPIO_PIN_SET);
+}
+
+
+int sx126x_brd_ctor(sx126x_board_t ** brd, void * user_arg)
+{
+	(void)user_arg;
+	*brd = NULL;
+
+	HAL_GPIO_WritePin(SX126X_NRST_GPIO_Port, SX126X_NRST_Pin, GPIO_PIN_SET);
+
+	HAL_GPIO_WritePin(SX126X_TXE_GPIO_Port, SX126X_TXE_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SX126X_RXE_GPIO_Port, SX126X_RXE_Pin, GPIO_PIN_RESET);
+
+	HAL_GPIO_WritePin(SX126X_CS_GPIO_Port, SX126X_CS_Pin, GPIO_PIN_SET);
+
+	return 0;
+}
+
+void sx126x_brd_dtor(sx126x_board_t * brd)
+{
+	(void)brd;
+}
+
+
+int sx126x_brd_get_time(sx126x_board_t * brd, uint32_t * value)
+{
+	*value = HAL_GetTick();
+	return 0;
+}
+
+
+int sx126x_brd_get_chip_type(sx126x_board_t * brd, sx126x_chip_type_t * chip_type)
+{
+	*chip_type = SX126X_CHIPTYPE_SX1268;
+	return 0;
+}
+
+
+int sx126x_brd_reset(sx126x_board_t * brd)
+{
+	HAL_GPIO_WritePin(SX126X_NRST_GPIO_Port, SX126X_NRST_Pin, GPIO_PIN_RESET);
+	HAL_Delay(10);
+	HAL_GPIO_WritePin(SX126X_NRST_GPIO_Port, SX126X_NRST_Pin, GPIO_PIN_SET);
+	return 0;
+}
+
+
+int sx126x_brd_wait_on_busy(sx126x_board_t * brd, uint32_t timeout)
+{
+	//HAL_Delay(100);
+	uint32_t start = HAL_GetTick();
+	while(1)
+	{
+		GPIO_PinState state = HAL_GPIO_ReadPin(SX126X_BUSY_GPIO_Port, SX126X_BUSY_Pin);
+		if (state == GPIO_PIN_RESET)
+			break;
+
+		uint32_t now = HAL_GetTick();
+		if (now - start >= timeout)
+			return SX126X_ERROR_TIMEOUT;
+	}
+
+	return 0;
+}
+
+
+int sx126x_brd_cleanup_irq(sx126x_board_t * brd)
+{
+	return 0;
+}
+
+
+void sx126x_brd_enable_irq(sx126x_board_t * brd)
+{
+	return;
+}
+
+
+void sx126x_brd_disable_irq(sx126x_board_t * brd)
+{
+	return;
+}
+
+
+int sx126x_brd_antenna_mode(sx126x_board_t * brd, sx126x_antenna_mode_t mode)
+{
+	int rc = 0;
+
+	HAL_GPIO_WritePin(SX126X_TXE_GPIO_Port, SX126X_TXE_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SX126X_RXE_GPIO_Port, SX126X_RXE_Pin, GPIO_PIN_RESET);
+
+	switch (mode)
+	{
+	case SX126X_ANTENNA_OFF:
+		break;
+
+	case SX126X_ANTENNA_TX:
+		HAL_GPIO_WritePin(SX126X_TXE_GPIO_Port, SX126X_TXE_Pin, GPIO_PIN_SET);
+		break;
+
+	case SX126X_ANTENNA_RX:
+		HAL_GPIO_WritePin(SX126X_RXE_GPIO_Port, SX126X_RXE_Pin, GPIO_PIN_SET);
+		break;
+
+	default:
+		rc = -1;
+	}
+
+	return rc;
+}
+
+
+int sx126x_brd_cmd_write(sx126x_board_t * brd, uint8_t cmd_code, const uint8_t * args, uint16_t args_size)
+{
+	_cs_down();
+
+	HAL_SPI_Transmit(hspi, &cmd_code, sizeof(cmd_code), HAL_MAX_DELAY);
+	HAL_SPI_Transmit(hspi, (uint8_t*)args, args_size, HAL_MAX_DELAY);
+
+	_cs_up();
+	return 0;
+}
+
+
+int sx126x_brd_cmd_read(sx126x_board_t * brd, uint8_t cmd_code, uint8_t * status, uint8_t * data, uint16_t data_size)
+{
+	uint8_t _status = 0xFF;
+	memset(data, 0xFF, data_size);
+
+	_cs_down();
+
+	HAL_SPI_Transmit(hspi, &cmd_code, sizeof(cmd_code), HAL_MAX_DELAY);
+	HAL_SPI_Receive(hspi, &_status, sizeof(_status), HAL_MAX_DELAY);
+	HAL_SPI_Receive(hspi, data, data_size, HAL_MAX_DELAY);
+
+	if (status) *status = _status;
+
+	_cs_up();
+	return 0;
+}
+
+
+int sx126x_brd_reg_write(sx126x_board_t * brd, uint16_t addr, const uint8_t * data, uint16_t data_size)
+{
+	const uint8_t cmd_code = SX126X_CMD_WRITE_REGISTER;
+
+	const uint8_t addr_bytes[2] = {
+			(addr >> 8) & 0xFF,
+			(addr >> 0) & 0xFF,
+	};
+
+	_cs_down();
+
+	HAL_SPI_Transmit(hspi, (uint8_t*)&cmd_code, sizeof(cmd_code), HAL_MAX_DELAY);
+	HAL_SPI_Transmit(hspi, (uint8_t*)addr_bytes, sizeof(addr_bytes), HAL_MAX_DELAY);
+	HAL_SPI_Transmit(hspi, (uint8_t*)data, data_size, HAL_MAX_DELAY);
+
+	_cs_up();
+
+	return 0;
+}
+
+
+int sx126x_brd_reg_read(sx126x_board_t * brd, uint16_t addr, uint8_t * data, uint16_t data_size)
+{
+	const uint8_t cmd_code = SX126X_CMD_READ_REGISTER;
+
+	const uint8_t addr_bytes[2] = {
+			(addr >> 8) & 0xFF,
+			(addr >> 0) & 0xFF,
+	};
+
+	uint8_t _status = 0xFF;
+	memset(data, 0xFF, data_size);
+
+	_cs_down();
+
+	HAL_SPI_Transmit(hspi, (uint8_t*)&cmd_code, sizeof(cmd_code), HAL_MAX_DELAY);
+	HAL_SPI_Transmit(hspi, (uint8_t*)addr_bytes, sizeof(addr_bytes), HAL_MAX_DELAY);
+	HAL_SPI_Receive(hspi, &_status, sizeof(_status), HAL_MAX_DELAY);
+	HAL_SPI_Receive(hspi, data, data_size, HAL_MAX_DELAY);
+
+	_cs_up();
+
+	return 0;
+}
+
+
+int sx126x_brd_buf_write(sx126x_board_t * brd, uint8_t offset, const uint8_t * data, uint8_t data_size)
+{
+	const uint8_t cmd_code = SX126X_CMD_WRITE_BUFFER;
+
+	_cs_down();
+
+	HAL_SPI_Transmit(hspi, (uint8_t*)&cmd_code, sizeof(cmd_code), HAL_MAX_DELAY);
+	HAL_SPI_Transmit(hspi, (uint8_t*)&offset, sizeof(offset), HAL_MAX_DELAY);
+	HAL_SPI_Transmit(hspi, (uint8_t*)data, data_size, HAL_MAX_DELAY);
+
+	_cs_up();
+
+	return 0;
+}
+
+
+int sx126x_brd_buf_read(sx126x_board_t * brd, uint8_t offset, uint8_t * data, uint8_t data_size)
+{
+	const uint8_t cmd_code = SX126X_CMD_READ_BUFFER;
+
+	uint8_t _status = 0xFF;
+	memset(data, 0xFF, data_size);
+
+	_cs_down();
+
+	HAL_SPI_Transmit(hspi, (uint8_t*)&cmd_code, sizeof(cmd_code), HAL_MAX_DELAY);
+	HAL_SPI_Transmit(hspi, (uint8_t*)&offset, sizeof(offset), HAL_MAX_DELAY);
+	HAL_SPI_Receive(hspi, (uint8_t*)&_status, sizeof(_status), HAL_MAX_DELAY);
+	HAL_SPI_Receive(hspi, (uint8_t*)data, data_size, HAL_MAX_DELAY);
+
+	_cs_up();
+
+	return 0;
+}
