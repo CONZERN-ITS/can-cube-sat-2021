7f5fca34d884b25a67075e6da831164530a7a278
SeresHotes <sereshotes@gmail.com>
2021-05-29 21:48:46 +0300

    Не тот файл был указан

--- src/ground/laptop/Ukaz/source/__init__.py
+++ src/ground/laptop/Ukaz/source/__init__.py
@@ -1,4 +1,4 @@
 import os
 RES_ROOT = os.path.dirname(os.path.realpath(__file__))
-USER_SETTINGS_PATH = os.path.join(os.path.split(RES_ROOT)[0], "UKAZ.yaml")
+USER_SETTINGS_PATH = os.path.join(os.path.split(RES_ROOT)[0], "Ukaz.yaml")
 LOG_FOLDER_PATH = os.path.join(os.path.split(RES_ROOT)[0], "log/")
