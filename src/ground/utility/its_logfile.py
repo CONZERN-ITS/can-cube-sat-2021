7cac35842769ff50562a7628e2d5fe872b609f8c
snork2 <strange.snork@gmail.com>
2021-06-19 20:30:10 +0300

    Логфайл поддерживает более точное время

--- src/ground/utility/its_logfile.py
+++ src/ground/utility/its_logfile.py
@@ -3,6 +3,8 @@ import contextlib
 import struct
 import time
 
+MAGIC = b"ZMQLOG"
+
 """ В этом файле лежат классы для записи в логфайл и чтения оттуда записей с ZMQ брокера
 	Записи представлены в виде List[bytes], где List - это мультипарт zmq сообщение,
 	а каждая отдельная запись bytes это собственно части мультипарт сообщения
@@ -17,9 +19,31 @@ import time
 """
 
 
-def _prim_header_size():
+def _probe_file_version(file_stream):
+	""" Проверка версии файла """
+	file_header_struct = struct.Struct("<6sL")
+	data = file_stream.read(file_header_struct.size)
+	magic, version_no = file_header_struct.unpack(data)
+	if magic != MAGIC:
+		return 0
+	else:
+		return version_no
+
+
+def _write_file_header(file_stream, version_no: int):
+	file_header_struct = struct.Struct("<6sL")
+	data = file_header_struct.pack(MAGIC, version_no)
+	file_stream.write(data)
+
+
+def _prim_header_size_v0():
 	""" размер первичного заголовка """
 	return 8 + 4  # uint64_t time, uin32_t records_count;
+
+
+def _prim_header_size_v1():
+	""" размер первичного заголовка """
+	return 8 + 4 + 4  # uint64_t seconds, uint32_t microseconds, uin32_t records_count;
 
 
 def _sec_header_size(parts_count):
@@ -27,28 +51,11 @@ def _sec_header_size(parts_count):
 	return parts_count * 4
 
 
-def _pack_message(msg_time: int, parts: typing.List[bytes]):
-	""" Запаковка сообщения в буфер для записи в файл
-		@param msg_time время получения сообщения
-		@param parts части сообщения
-	"""
-
-	# Пакуем время и количество записей
-	data = struct.pack("<QI", msg_time, len(parts))
-	# Дальше пакуем список длин
-	data += struct.pack("<%dI" % len(parts), *[len(x) for x in parts])
-	# а дальше просто приклеиваем записи
-	for part in parts:
-		data += part
-
-	return data
-
-
-def _unpack_message_prim_header(header_bytes: bytes):
+def _unpack_message_prim_header_v0(header_bytes: bytes):
 	""" Распаковка заголовка сообщения. Заголовок это uint64_t время
 		и uint32_t количество частей в нем """
 
-	if len(header_bytes) != _prim_header_size():
+	if len(header_bytes) != _prim_header_size_v0():
 		# Это не правильный заголовок
 		raise ValueError(
 			"Invalid ITS ZMQ LOG header size: %s. %s were expected"
@@ -56,6 +63,22 @@ def _unpack_message_prim_header(header_bytes: bytes):
 		)
 
 	msg_time, parts_count = struct.unpack("<QI", header_bytes)
+	return msg_time, parts_count
+
+
+def _unpack_message_prim_header_v1(header_bytes: bytes):
+	""" Распаковка заголовка сообщения. Заголовок это uint64_t время
+		и uint32_t количество частей в нем """
+
+	if len(header_bytes) != _prim_header_size_v1():
+		# Это не правильный заголовок
+		raise ValueError(
+			"Invalid ITS ZMQ LOG header size: %s. %s were expected"
+			% (len(bytes), 8 + 4)
+		)
+
+	time_s, time_us, parts_count = struct.unpack("<QLI", header_bytes)
+	msg_time = float(time_s) + time_us / 10**6
 	return msg_time, parts_count
 
 
@@ -96,25 +119,78 @@ def _unpack_message_parts(parts_sizes, parts_bytes):
 
 class LogfileWriter(contextlib.AbstractContextManager):
 
-	def __init__(self, filepath=None, stream=None):
+	def __init__(self, filepath=None, stream=None, version=1):
 		super(LogfileWriter, self).__init__()
 		self.stream = None
 		self.owns_stream = False
+		self.version = version
+		if self.version == 0:
+			self._packer = self._pack_v0
+		elif self.version == 1:
+			self._packer = self._pack_v1
+		else:
+			raise ValueError("Unsupported version %d" % version)
 
 		if filepath:
 			self.open(filepath)
 		elif stream:
 			self.open_stream(stream)
 
+	def _prepare_stream(self):
+		if self.version > 0:
+			_write_file_header(self.stream, self.version)
+
+	def _pack_v0(self, msg_time, parts):
+		""" Запаковка сообщения в буфер для записи в файл версии 0
+			@param msg_time время получения сообщения
+			@param parts части сообщения
+		"""
+		time_s = int(msg_time)
+
+		# Пакуем время и количество записей
+		data = struct.pack("<QI", time_s, len(parts))
+		# Дальше пакуем список длин
+		data += struct.pack("<%dI" % len(parts), *[len(x) for x in parts])
+		# а дальше просто приклеиваем записи
+		for part in parts:
+			data += part
+
+		return data
+
+	def _pack_v1(self, msg_time, parts):
+		""" Запаковка сообщения в буфер для записи в файл версии 1
+			@param msg_time время получения сообщения
+			@param parts части сообщения
+		"""
+		if isinstance(msg_time, int):
+			time_s = msg_time
+			time_us = 0
+		elif isinstance(msg_time, float):
+			time_s = int(msg_time)
+			time_us = (msg_time - time_s) * 10**6
+			time_us = int(time_us)
+
+		# Пакуем время и количество записей
+		data = struct.pack("<QLI", time_s, time_us, len(parts))
+		# Дальше пакуем список длин
+		data += struct.pack("<%dI" % len(parts), *[len(x) for x in parts])
+		# а дальше просто приклеиваем записи
+		for part in parts:
+			data += part
+
+		return data
+
 	def open(self, filepath):
 		self.close()
 		self.stream = open(filepath, mode="wb")
 		self.owns_stream = False
+		self._prepare_stream()
 
 	def open_stream(self, stream):
 		self.close()
 		self.stream = stream
 		self.owns_stream = True
+		self._prepare_stream()
 
 	def close(self):
 		if self.stream and self.owns_stream:
@@ -124,7 +200,8 @@ class LogfileWriter(contextlib.AbstractContextManager):
 		self.owns_stream = False
 
 	def write(self, zmq_multipart_message: typing.List[bytes]):
-		msg_bytes = _pack_message(int(time.time()), zmq_multipart_message)
+
+		msg_bytes = self._packer(time.time(), zmq_multipart_message)
 		self.stream.write(msg_bytes)
 
 	def write_all(self, msg_list: typing.List[typing.List[bytes]]):
@@ -144,21 +221,40 @@ class LogfileReader(contextlib.AbstractContextManager):
 		super(LogfileReader, self).__init__()
 		self.stream = None
 		self.owns_stream = False
+		self.version = None
+		self._prim_header_unpacker = None
+		self._prim_header_size = None
 
 		if filepath:
 			self.open(filepath)
 		elif stream:
 			self.open_stream(stream)
 
+	def _fetch_version(self):
+		version = _probe_file_version(self.stream)
+		if version == 0:
+			self.stream.seek(0)
+			self._prim_header_unpacker = _unpack_message_prim_header_v0
+			self._prim_header_size = _prim_header_size_v0()
+		elif version == 1:
+			self._prim_header_unpacker = _unpack_message_prim_header_v1
+			self._prim_header_size = _prim_header_size_v1()
+		else:
+			raise ValueError("Unsupported file version %d" % version)
+
+		self.version = version
+
 	def open(self, filepath):
 		self.close()
 		self.stream = open(filepath, mode="rb")
 		self.owns_stream = True
+		self._fetch_version()
 
 	def open_stream(self, stream):
 		self.close()
 		self.stream = stream
 		self.owns_stream = False
+		self._fetch_version()
 
 	def close(self):
 		if self.stream and self.owns_stream:
@@ -171,7 +267,7 @@ class LogfileReader(contextlib.AbstractContextManager):
 
 	def read(self):
 		# Читаем первичный заголовок
-		prim_header_size = _prim_header_size()
+		prim_header_size = self._prim_header_size
 		prim_header_bytes = self.stream.read(prim_header_size)
 		# Возможно файл кончился?
 		if not prim_header_bytes:
@@ -180,8 +276,7 @@ class LogfileReader(contextlib.AbstractContextManager):
 		if len(prim_header_bytes) != prim_header_size:
 			raise ValueError("Unexpected EOF in primary header")
 
-		msg_time, parts_count = _unpack_message_prim_header(prim_header_bytes)
-
+		msg_time, parts_count = self._prim_header_unpacker(prim_header_bytes)
 		# Читаем вторичный заголовок
 		sec_header_size = _sec_header_size(parts_count)
 		parts_sizes_bytes = self.stream.read(sec_header_size)
@@ -214,6 +309,7 @@ class LogfileReader(contextlib.AbstractContextManager):
 
 def test():
 	filepath = "/tmp/testfile.itslog"
+
 	msgs = [
 		[b"hurdurdur", b"ramramram", b"vetvetvet"],
 		[b"", b"", b""],
@@ -221,13 +317,13 @@ def test():
 		[b"", b"321", b"cadebaba"],
 	]
 
-	with LogfileWriter(filepath) as writer:
+	with LogfileWriter(filepath, version=1) as writer:
 		writer.write_all(msgs)
 
 	with LogfileReader(filepath) as reader:
 		rmsgs = reader.read_all()
-		for _, rmsg in rmsgs:
-			print(rmsg)
+		for time, rmsg in rmsgs:
+			print(time, rmsg)
 
 
 # if __name__ == "__main__":
