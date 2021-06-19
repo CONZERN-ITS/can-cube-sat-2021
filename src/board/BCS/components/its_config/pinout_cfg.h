8fcc1307daa935824c304f4cd04fb7bdb96c554f
SeresHotes <sereshotes@gmail.com>
2021-06-19 22:10:50 +0300

    Оказывается, порядок сдивговых регистров другой :(

--- src/board/BCS/components/its_config/pinout_cfg.h
+++ src/board/BCS/components/its_config/pinout_cfg.h
@@ -33,37 +33,37 @@
 #define ITS_PIN_OWB			27
 
 //-------------------------------------------
-#define ITS_PIN_SR_BSK1_VCC  1
-#define ITS_PIN_SR_BSK2_VCC  5
-#define ITS_PIN_SR_BSK3_VCC  9
-#define ITS_PIN_SR_BSK4_VCC  13
+#define ITS_PIN_SR_BSK1_VCC  9
+#define ITS_PIN_SR_BSK2_VCC  13
+#define ITS_PIN_SR_BSK3_VCC  25
+#define ITS_PIN_SR_BSK4_VCC  29
 
-#define ITS_PIN_SR_SD_VCC    25
-#define ITS_PIN_SR_RADIO_VCC 26
-#define ITS_PIN_SR_SINS_VCC  17
-#define ITS_PIN_SR_PL1_VCC   18
-#define ITS_PIN_SR_PL2_VCC   19
-#define ITS_PIN_SR_PL3_VCC   20
-#define ITS_PIN_SR_PL4_VCC   21
-#define ITS_PIN_SR_PL5_VCC   22
-#define ITS_PIN_SR_PL6_VCC   23
+#define ITS_PIN_SR_SD_VCC    17
+#define ITS_PIN_SR_RADIO_VCC 18
+#define ITS_PIN_SR_SINS_VCC  1
+#define ITS_PIN_SR_PL1_VCC   2
+#define ITS_PIN_SR_PL2_VCC   3
+#define ITS_PIN_SR_PL3_VCC   4
+#define ITS_PIN_SR_PL4_VCC   5
+#define ITS_PIN_SR_PL5_VCC   6
+#define ITS_PIN_SR_PL6_VCC   7
 
-#define ITS_PIN_SR_BSK1_HEAT 0
-#define ITS_PIN_SR_BSK2_HEAT 4
-#define ITS_PIN_SR_BSK3_HEAT 8
-#define ITS_PIN_SR_BSK4_HEAT 12
-#define ITS_PIN_SR_BSK5_HEAT 16
-#define ITS_PIN_SR_BCU_HEAT  24
+#define ITS_PIN_SR_BSK1_HEAT 8
+#define ITS_PIN_SR_BSK2_HEAT 12
+#define ITS_PIN_SR_BSK3_HEAT 24
+#define ITS_PIN_SR_BSK4_HEAT 28
+#define ITS_PIN_SR_BSK5_HEAT 0
+#define ITS_PIN_SR_BCU_HEAT  16
 
-#define ITS_PIN_SR_BSK1_ROZE 2
-#define ITS_PIN_SR_BSK2_ROZE 6
-#define ITS_PIN_SR_BSK3_ROZE 10
-#define ITS_PIN_SR_BSK4_ROZE 14
+#define ITS_PIN_SR_BSK1_ROZE 10
+#define ITS_PIN_SR_BSK2_ROZE 14
+#define ITS_PIN_SR_BSK3_ROZE 26
+#define ITS_PIN_SR_BSK4_ROZE 30
 
-#define ITS_PIN_SR_BSK1_CMD  3
-#define ITS_PIN_SR_BSK2_CMD  7
-#define ITS_PIN_SR_BSK3_CMD  11
-#define ITS_PIN_SR_BSK4_CMD  15
+#define ITS_PIN_SR_BSK1_CMD  11
+#define ITS_PIN_SR_BSK2_CMD  15
+#define ITS_PIN_SR_BSK3_CMD  27
+#define ITS_PIN_SR_BSK4_CMD  31
 
 
 #endif /* MAIN_PINOUT_CFG_H_ */
