f6a2b5db485482bc75a3857c675a7cc40ea8779d
snork2 <strange.snork@gmail.com>
2021-07-12 17:15:59 +0300

    Кто-то не умеет вызывать функции. Должно исправлять не взятие времени gps https://conzern-its.myjetbrains.com/youtrack/issue/CCS2021-131

--- src/board/SINS/Core/Src/stm32f4xx_it.c
+++ src/board/SINS/Core/Src/stm32f4xx_it.c
@@ -243,7 +243,7 @@ void SysTick_Handler(void)
 void EXTI0_IRQHandler(void)
 {
   /* USER CODE BEGIN EXTI0_IRQn 0 */
-  void gps_pps_signal_handler(void);
+  gps_pps_signal_handler();
   /* USER CODE END EXTI0_IRQn 0 */
   HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
   /* USER CODE BEGIN EXTI0_IRQn 1 */
